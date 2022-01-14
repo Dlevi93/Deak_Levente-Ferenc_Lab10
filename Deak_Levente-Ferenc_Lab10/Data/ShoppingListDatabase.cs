804043526eb0f193c9c148693d5056674d56a9f4
Deak Levente <Dlevi93@outlook.com>
2022-01-14 18:32:40 +0200

    Add project files.

---
+++ Deak_Levente-Ferenc_Lab10/Data/ShoppingListDatabase.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Models;
+using SQLite;
+
+namespace Data
+{
+    public class ShoppingListDatabase
+    {
+        readonly SQLiteAsyncConnection _database;
+
+        public ShoppingListDatabase(string dbPath)
+        {
+            _database = new SQLiteAsyncConnection(dbPath);
+            _database.CreateTableAsync<ShopList>().Wait();
+        }
+        public Task<List<ShopList>> GetShopListsAsync()
+        {
+            return _database.Table<ShopList>().ToListAsync();
+        }
+        public Task<ShopList> GetShopListAsync(int id)
+        {
+            return _database.Table<ShopList>().Where(i => i.ID == id).FirstOrDefaultAsync();
+        }
+        public Task<int> SaveShopListAsync(ShopList slist)
+        {
+            return slist.ID != 0 ? _database.UpdateAsync(slist) : _database.InsertAsync(slist);
+        }
+        public Task<int> DeleteShopListAsync(ShopList slist)
+        {
+            return _database.DeleteAsync(slist);
+        }
+    }
+}
---
+++ Deak_Levente-Ferenc_Lab10/ListPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Deak_Levente_Ferenc_Lab10;
+using Models;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Deak_LeventeFerenc_Lab10
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ListPage : ContentPage
+    {
+        public ListPage()
+        {
+            InitializeComponent();
+        }
+
+        async void OnSaveButtonClicked(object sender, EventArgs e)
+        {
+            var slist = (ShopList)BindingContext;
+            slist.Date = DateTime.UtcNow;
+            await App.Database.SaveShopListAsync(slist);
+            await Navigation.PopAsync();
+        }
+        async void OnDeleteButtonClicked(object sender, EventArgs e)
+        {
+            var slist = (ShopList)BindingContext;
+            await App.Database.DeleteShopListAsync(slist);
+            await Navigation.PopAsync();
+        }
+    }
+}
